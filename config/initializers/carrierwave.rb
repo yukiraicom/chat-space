b36802f96c2198977d2c7066240876b0cd309d28
yukiraicom <araiyuuki@araiyuukinoMacBook.local>
2018-05-22 13:54:21 +0900

    deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-sooutheast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
+  config.fog_directory  = 'upload-test-yuki'
   config.asset_host = 'https://s3-ap-southeast-1'
 end
