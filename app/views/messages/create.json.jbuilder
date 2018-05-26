204abba18ad02788b50d0a2574c574c3d3c63912
yukiraicom <araiyuuki@araiyuukinoMacBook.local>
2018-05-26 22:38:22 +0900

    edit image

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
 json.name @message.user.name
 json.content @message.content
 json.id @message.id
+json.image @message.image
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-southeast-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'upload-test-yuki'
-  config.asset_host = 'https://s3-ap-southeast-1.amazonaws.com/upload-test-yuki'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test-yuki'
 end
